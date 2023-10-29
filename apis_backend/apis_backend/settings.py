b5b67737dbb66844e83d8da4145e057751e8beb9
Jorge Ktch <jorgektch@gmail.com>
2023-10-28 21:32:16 -0500

    Updated

--- apis_backend/apis_backend/settings.py
+++ apis_backend/apis_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o2yubtys8m6(dwc-891n%wum_&=0(*p1%*kk5&c$@82*(hg+qx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
